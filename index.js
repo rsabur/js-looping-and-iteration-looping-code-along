ae7a2a670a286a2b322601ae1c325e7b0fd31dae
Radiyah <rsabur58@gmail.com>
2021-04-04 17:55:08 -0400

    commit tree c45735f54148aabb96bebedd54a527ec513a9f2d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let thankYouCards = []
+    for (let i = 0; i<names.length; i++){
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    let countDown = number;
+        while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
